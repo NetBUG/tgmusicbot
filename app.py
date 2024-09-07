cb71390b4d199bf914f1a1edecb289ba4df6fb3b
NetBUG <o.urzhumtsev@sdventures.com>
2024-09-08 01:01:20 +0200

    Added handling of existing files (including same size)

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if not TOKEN:
 bot = telebot.TeleBot(TOKEN, parse_mode="MARKDOWN") # You can set parse_mode by default. HTML or MARKDOWN
 
 # See https://github.com/Delgan/loguru?tab=readme-ov-file#easier-file-logging-with-rotation-retention-compression
-logger.add("file_{time}.log", format="{time} {level} {message}") # , compression="zip"
+logger.add("tgmusicbot.log", format="{time:YYYY-MM-DD HH:mm} {level} {message}") # , compression="zip", rotation="10 MB"
 
 logger.info("Started bot!")
 
@@ -26,13 +26,20 @@ def handle_audio(file, artist, title):
     if not os.path.isdir(audio_path):
         os.makedirs(audio_path, exist_ok=True)
     audio_file = os.path.join(audio_path, title)
+    counter = 0
+    filename = title
+    while os.path.exists(audio_file):
+        if os.path.getsize(audio_file) == len(file):
+            logger.info(f"File {audio_file} already exists but it's the same file!")
+            break
+        logger.warning(f"File {audio_file} already exists!")
+        filename = title.replace(".", "_" + str(counter) + ".")
+        audio_file = os.path.join(audio_path, filename)
+        counter += 1
     with open(audio_file, 'wb') as new_file:
         new_file.write(file)
-    return audio_file
+    return audio_file, counter != 0
 
-# TODO append a logfile
-# TODO timestamp - readable
-# TODO handle existing files
 # TODO handle other types and say "sorry"?
 
 @bot.message_handler(commands=['start', 'help'])
@@ -45,15 +52,13 @@ def handle_docs_audio(message):
     # See https://github.com/eternnoir/pyTelegramBotAPI/blob/master/examples/download_file_example.py
     if message.audio:
         file_info = bot.get_file(message.audio.file_id)
-        # print("File: ")
-        # print(file_info)
         downloaded_file = bot.download_file(file_info.file_path)
-        # print(downloaded_file)
-        saved_path = handle_audio(downloaded_file, message.audio.performer, message.audio.file_name)
-        logger.info(f"Saved as {saved_path}!")
-        bot.reply_to(message, f"Saved as {saved_path}!")
+        saved_path, overwritten = handle_audio(downloaded_file, message.audio.performer, message.audio.file_name)
+        response_msg = f"Saved as {saved_path}!" if overwritten else f"Already exists: saved as {saved_path}!"
+        logger.info(response_msg)
+        bot.reply_to(message, response_msg.replace('_', '\\_'))
         return
-    print(message)
+    logger.error(message)
     bot.reply_to(message, "Saving failed!")
 
 @bot.message_handler(func=lambda m: True)
